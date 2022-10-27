a378b1c9f12e1209ddd18f2e76d0eb221afaf0b0
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-10-27 09:38:01 -0400

    feat: configured connection to database

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+using System.Data.SqlClient;
+using TodoApi.Data;
+
+namespace TodoApi.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        builder.Services.AddScoped<TaskContext.GetConnection>(sp =>
+            async () =>
+            {
+                var connection = new SqlConnection(connectionString);
+                await connection.OpenAsync();
+                return connection;
+            });
+        return builder;
+    }
+}
